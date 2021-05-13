d02e9b5fd1f50dd310b9929a1d8304002fc5e989
MackRoe <elaine.music@students.makeschool.com>
2021-05-13 18:57:12 -0400

    build resolvers, app start, and sample data

--- server.js
+++ server.js
@@ -4,11 +4,43 @@ const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
 
 // Schema
-const schema = buildSchema(
-    type Todo{
+const schema = buildSchema(`
+    type Todo {
         name: String!
-        completed: Boolean
+        completed: String
         date: String
         id: Int
     }
-)
+
+    type Query {
+        getAllTodos: [Todo]
+        getTodo: Todo
+        getCompletedTodos: [Todo]
+    }`)
+
+// starting data
+const data = [
+	{ name: 'Sample Item', completed: 'True', date: new Date(), id: 1 }
+]
+
+// Resolvers
+const root = {
+    getAllTodos: () => {
+        return data
+    }
+}
+
+const app = express()
+
+// GraphQL Route
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+}))
+
+// Start the app
+const port = 4000
+app.listen(port, () => {
+    console.log('ToDo-API running on port: ${port}')
+})
